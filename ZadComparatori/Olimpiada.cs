8590d7c3e03143572a8ff904576271b2a8cc8794
Svetlana Georgieva <sv.georgieva@pgzaimov.com>
2023-02-22 11:25:39 +0200

    first commit

---
+++ ZadComparatori/Olimpiada.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ZadComparatori
+{
+    internal class Olimpiada:IPrint,IComparable<Olimpiada>
+    {
+        private string nameOlimpiada;
+        private int brTochki;
+
+        public string NameOlimpiada
+        {
+            get { return nameOlimpiada; }
+            set
+            {
+                nameOlimpiada = value;
+            }
+        }
+        public int BrTochki
+        {
+            get { return brTochki; }
+            set { brTochki = value; }
+        }
+        public Olimpiada(string nameOlimpiada, int brTochki)
+        {
+            NameOlimpiada = nameOlimpiada;
+            BrTochki = brTochki;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.nameOlimpiada}-->{brTochki}");
+        }
+
+        public int CompareTo(Olimpiada other)
+        {
+            return brTochki.CompareTo(other.brTochki);
+        }
+    }
+}
---
+++ ZadComparatori/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+
+namespace ZadComparatori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Olimpiada olimp1 = new Olimpiada("IT", 98);
+            Olimpiada olimp2 = new Olimpiada("Math", 100);
+            List<Olimpiada> olimpiadas = new List<Olimpiada>
+            {
+                olimp1,
+                olimp2
+            };
+            olimpiadas.Sort();
+            var comparator = new Comparator();
+            olimpiadas.Sort(comparator);
+            Console.WriteLine(string.Join(", ", olimpiadas.Select(x=>x.BrTochki)));
+            Console.WriteLine(string.Join(", ", olimpiadas.Select(x => x.NameOlimpiada)));
+        }
+    }
+}
